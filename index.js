e94ec3a0a799ddf2628d76e01c80456c68b6028b
Patrick Sturm <p.sturm@spherical-elephant.com>
2017-08-01 18:07:16 +0200

    fixed up /entity POST added untested /entity GET

--- index.js
+++ index.js
@@ -3,38 +3,65 @@
 const express = require('express');
 
 module.exports = (model, opts) => {
-    const router = express.Router();
-
     if (!model) throw new Error('model must be set!');
-
+    opts = opts || {};
+    
+    const router = express.Router();
     const route = opts.route || model.name;
 
-    const _attachReply = (req, res, next, statusCode, result) => {
+    const _attachReply = (req, res, next, statusCode, result, message) => {
+        // TODO: check if required fields are set!
         req.custom = {
             statusCode,
-            result
+            result,
+            message
         };
         return next();
     };
 
+    const _attachErrorReply = (req, res, next, statusCode, result, message) => {
+        return next({
+            statusCode,
+            result,
+            message
+        });
+    };
+
     router.post('/', (req, res, next) => {
-        const attachReply = _attachReply(req, res, next);
+        const attachReply = _attachReply.bind(null, req, res, next);
+        const attachErrorReply = _attachErrorReply.bind(null, req, res, next);
+
         model
             .create(req.body)
             .then(modelInstance => {
                 attachReply(201, modelInstance.get({ plain: true }));
             }).catch(err => {
-                attachReply(500, modelInstance.get({ plain: true }));
+                // TODO: validation errors cause different exception
+                attachErrorReply(500, err);
             });
     });
 
     router.get('/', (req, res, next) => {
-        const startPage = req.query.p;
-        const itemsPerPage = req.query.i || 10;
+        const attachReply = _attachReply.bind(null, req, res, next);
+        const attachErrorReply = _attachErrorReply.bind(null, req, res, next);
 
-        const attachReply = _attachReply(req, res, next);
+        const limit = req.query.p;
+        const offset = req.query.i;
+        const attributes = (req.query.a || '').split('|');
+        const order = [(req.query.s || '').split('|')];
 
+        if ((!limit || !offset) && limit !== offset)
+            return attachReply(400, undefined, 'p or i must be both undefined or both defined.');
 
+        if ((limit && isNaN(parseInt(limit))) || (offset && isNaN(parseInt(offset))))
+            return attachReply(400, undefined, 'p or i are not a number!');
+
+        model
+            .findAll({ limit, offset, attributes, order })
+            .then(modelInstances => {
+            }).catch(err => {
+                attachErrorReply(500, err);
+            });
     });
 
     router.get('/:id', (req, res, next) => {
