ce9f54b0d3a66bc090b9d3fb7c7c2ad1b123fdbb
Patrick Sturm <siyb@geekosphere.org>
2020-05-29 10:35:28 +0200

    restructuring version specific test code

---
+++ test/all-versions.test.js
@@ -0,0 +1,14 @@
+'use strict';
+const versions = {};
+['sequelize4', 'sequelize5', 'sequelize6'].forEach(version => {
+  describe(version, () => {
+    versions[version] = require(version);
+    console.log('testing sequelize version', versions[version].version);
+    require('./index.test')(versions[version]);
+    require('./lib/openapi/sequelize/model-converter.test')(versions[version]);
+    require('./lib/data-mapper/sequelize/operator-table.test');
+    require('./lib/data-mapper/sequelize/query-builder.test')(versions[version]);
+    require('./lib/route/route-exposure-handler.test');
+  });
+});
+
